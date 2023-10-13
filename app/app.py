da0fec213a13660dd23f4c5ebb8fc31c8d47b8c0
n1khilmane <nikhilmane1919@gmail.com>
2023-10-13 11:07:28 -0400

    wip, app.py, added barplots code

--- app/app.py
+++ app/app.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template
 from markupsafe import escape
 import pandas as pd
 
+import matplotlib
+matplotlib.use('Agg')  # Set Matplotlib to use a non-GUI backend
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import math
+import numpy
+
 
 import db.db as dbm
 from db.db import PackageType, packge_type_exists
@@ -137,6 +145,79 @@ def show_packages_(package_type):
         return "Package type not found"
 
     return render_template(template_name, records=records)
+
+
+
+def make_barplot2ylog(title, barlabels,
+                      barlabel_to_C1, C1_label, C1_color,
+                      barlabel_to_C2, C2_label, C2_color, Cmax=None):
+    c1_vals = []
+    c2_vals = []
+    Cmax0 = 0
+    for label in barlabels:
+        C1 = barlabel_to_C1[label]
+        if C1 > Cmax0:
+            Cmax0 = C1
+        c1_vals.append(math.log10(1 + C1))
+        C2 = barlabel_to_C2[label]
+        if C2 > Cmax0:
+            Cmax0 = C2
+        c2_vals.append(math.log10(1 + C2))
+    xticks = numpy.arange(len(c1_vals)) + 0.5
+    width = 0.40  # the width of the bars
+
+    fig, ax = plt.subplots(figsize=(9, 5))  # Create a figure and axis
+    rects2 = ax.bar(xticks, c2_vals, width, align='edge', color=C2_color)
+    rects1 = ax.bar(xticks, c1_vals, -width, align='edge', color=C1_color)
+    xlabels = [label.replace('/', '\n') for label in barlabels]
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels)
+
+    if Cmax == None:
+        Cmax = Cmax0
+    if Cmax < 100:
+        nb_pow10ticks = 3
+    else:
+        nb_pow10ticks = int(math.log10(Cmax)) + 2
+
+    # Add labels, legend, and adjust plot settings as needed
+
+    # Convert the plot to a PNG image
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close(fig)  # Close the figure to release resources
+
+    # Encode the PNG image as base64
+    image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+
+    return image_base64
+
+
+@app.route('/packages/stats/bioc/BiocVersion/')
+def index5():
+    # Dummy data - Replace with your data retrieval logic
+    barlabels = ["Jan/2023", "Feb/2023", "Mar/2023", "Apr/2023", "May/2023", "Jun/2023", "Jul/2023", "Aug/2023", "Sep/2023", "Oct/2023", "Nov/2023", "Dec/2023"]
+    barlabel_to_C1 = {"Jan/2023": 3165198, "Feb/2023": 3488622, "Mar/2023": 4407007, "Apr/2023": 3892123, "May/2023": 4029362, "Jun/2023": 3623651, "Jul/2023": 3531188, "Aug/2023": 3718150, "Sep/2023": 3509979, "Oct/2023": 835681, "Nov/2023": 0, "Dec/2023": 0}
+    barlabel_to_C2 = {"Jan/2023": 96471, "Feb/2023": 108380, "Mar/2023": 134470, "Apr/2023": 126179, "May/2023": 127681, "Jun/2023": 122621, "Jul/2023": 122530, "Aug/2023": 121541, "Sep/2023": 130785, "Oct/2023": 43192, "Nov/2023": 0, "Dec/2023": 0}
+
+    title = "Download Stats for Bioconductor Software Repository (2023)"
+    
+    image_base64 = make_barplot2ylog(title, barlabels, barlabel_to_C1, 'C1 Label', 'blue', barlabel_to_C2, 'C2 Label', 'red')
+
+    return render_template('stats-bioc.html', image_base64=image_base64)
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
