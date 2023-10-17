d59b015abaf977d907090eda0c5a06bd0265c9ae
Robert D Shear <rshear@gmail.com>
2023-10-17 06:36:29 -0400

    ETL Source operational

--- etl/bioc_web_downloads.sql
+++ etl/bioc_web_downloads.sql
@@ -1,22 +1,40 @@
-CREATE OR REPLACE VIEW bioc_web_downloads AS
- with T1 as (
-    select "date", "c-ip", "sc-status", "cs-uri-stem", 
-    regexp_extract("cs-uri-stem", '^/+packages/(?:[^/]*)/+([^/]*)/+([^/]*)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$', 1) path1,
-    regexp_extract("cs-uri-stem", '^/+packages/(?:[^/]*)/+([^/]*)/+([^/]*)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$', 2) path2,
-    regexp_extract("cs-uri-stem", '^/+packages/(?:[^/]*)/+([^/]*)/+([^/]*)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$', 3) package
-    from "bioc_web_logs"
-    where "sc-status" between 200 and 399 and 
-    regexp_like("cs-uri-stem", '^/packages/(?:[^/]*)/([^/]*)/([^/]*)/(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$')
-), 
+/*
+view bioc_web_downloads
+package and categories are inferred from the cs-uri-stem
+Select only package downloads, which must have a valid category and end with a package tarball
+The package tarball must be in the form 
+    :<package_name>_<version>.tar.<compression>,
+    where compression is gz, zip, or tgz
+The category is extracted from the start of the cs-uri-stem, and must be in the form:
+    /packages/<version>/<category>/...
+Valid categories are: bioc, workflows, data/experiment, data/annotation
 
-T2 as (
-select "date", "c-ip", "sc-status", "cs-uri-stem", 
-    case
-        when path1 in ('bioc', 'workflows') then path1
-        when path1 = 'data'
-            then path2
-        else ''
-    end "category",
-    "package"
-from T1)
-select year("date") "year", month("date") "month", * from T2 where "category" <> ''
+Note: Wherever a '//' appears in the uri-stem, it is treated as a single '/'
+Note: The RE string is repeated multiple times becasue Athena does not have reasonable way
+to handle manifest constants (as of Oct-2023) 
+
+*/
+CREATE OR REPLACE VIEW "bioc_web_downloads" AS
+SELECT "date",
+    "c-ip",
+    "sc-status",
+    regexp_extract(
+        "cs-uri-stem",
+        '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$',
+        1
+    ) category,
+    regexp_extract(
+        "cs-uri-stem",
+        '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$',
+        2
+    ) package
+FROM "bioc_web_logs"
+WHERE (
+        (
+            "sc-status" BETWEEN 200 AND 399
+        )
+        AND regexp_like(
+            "cs-uri-stem",
+            '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:[^/]*/)*([^_]*)_.*tar\.(?:gz|zip|tgz)$'
+        )
+    )
