193e1404eb2f2f3c4cd729901e5d83c443a0e1a6
n1khilmane <nikhilmane1919@gmail.com>
2023-10-19 05:53:43 -0400

    WIP test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,18 +7,20 @@ import db.db as dbm
 PATH = '/packages/stats'
 
 def test_show_packages_endpoint(client):
+    
+    #Arrange
     # Create a test client using the pytest-flask fixture
     client = app.test_client()
 
+    #Act
     # Simulate a call to the endpoint
     response = client.get(PATH + '/bioc/bioc_packages.txt')
 
+    #Assert
     # Check the HTTP status code
     assert response.status_code == 200
-
     # Check the content type
     assert response.content_type == 'text/plain; charset=utf-8'
-
     # Assuming db.get_package_names() returns a dictionary with a 'package' key
     # Check if the response content matches the expected result
     expected_text = '\n'.join([row for row in dbm.get_package_names()['package']])
