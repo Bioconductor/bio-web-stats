e7cdcd4fce287e6260ced03023311e10a614830c
Robert Shear <rshear@gmail.com>
2023-10-26 10:23:26 -0400

    bug in populate_download_summary query

--- etl/i_populate_download_summary.sql
+++ etl/i_populate_download_summary.sql
@@ -15,9 +15,9 @@ select "category", "package",
 UNION ALL
 select category,
     package,
-    date_trunc('MONTH', "date") "date",
+    date_trunc('MONTH', "date") "date", 
     1 as is_monthly,
-    count("c-ip") ip_count,
+    count(distinct "c-ip") ip_count,
     count(*) download_count
 from bioc_web_downloads
 group by category,
