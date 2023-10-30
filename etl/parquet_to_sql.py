4f4f5593bc82e61e6deeb9f6d920bd17e5a1cacd
Robert D Shear <rshear@gmail.com>
2023-10-30 07:18:59 -0400

    Parquet-to-sqlite3 conversion tools

---
+++ etl/parquet_to_sql.py
@@ -0,0 +1,32 @@
+# pip install pandas fastparquet
+# for f in /path/to/directory/*; do mv "$f" "$f.parquet"; done
+"""utility for converting parquet files to sqlite3.
+
+1. download the parquet files from S3
+2. add the file extensions:
+for f in /path/to/directory/*; do mv "$f" "$f.parquet"; done
+3. Run this script
+"""
+import pandas as pd
+import fastparquet
+
+"""
+CREATE TABLE stats (
+        category TEXT, 
+        package TEXT, 
+        date DATE, 
+        ip_count BIGINT, 
+        download_count BIGINT)
+"""
+
+from sqlalchemy import create_engine
+
+df = pd.read_parquet('/Users/robertshear/temp/parquet/', engine='fastparquet')
+df['date'] = df['date'].dt.date
+
+# Create engine
+# TODO must be in specific directory
+engine = create_engine('sqlite:///dev.db')
+
+# Write DataFrame to database
+df.to_sql('stats', engine, index=False, if_exists='replace')
