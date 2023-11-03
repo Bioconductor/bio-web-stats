21dac32ff27029cc0da83fd76bd373e259b6a810
n1khilmane <nikhilmane1919@gmail.com>
2023-11-03 01:09:02 -0400

    wip test-instance-config.py, .env

---
+++ test-instance-config.py
@@ -0,0 +1,15 @@
+from decouple import config
+
+# Flask configuration
+ENV = config('FLASK_ENV', default='production')
+DEBUG = ENV == 'development'
+SEND_FILE_MAX_AGE_DEFAULT = 0
+DEBUG_TB_ENABLED = DEBUG
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+# SQLAlchemy configuration
+SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI', default='sqlite:///development.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# Cache configuration
+CACHE_TYPE = config('CACHE_TYPE', default='SimpleCache')
