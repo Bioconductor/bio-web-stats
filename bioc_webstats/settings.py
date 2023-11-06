cb7e5c0bbc379f88280db5c2d8506fd558e7ca54
n1khilmane <nikhilmane1919@gmail.com>
2023-11-06 13:36:14 -0500

    wip TODO: excluding splash from the production

--- bioc_webstats/settings.py
+++ bioc_webstats/settings.py
@@ -21,4 +21,3 @@ DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = "SimpleCache"  # Can be "MemcachedCache", "RedisCache", etc.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 URI_PATH_PREFIX = "/packages/stats"
-
