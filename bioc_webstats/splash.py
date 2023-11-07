7ceead4b78bff905b115d94829c4ef1ce42aa7ca
n1khilmane <nikhilmane1919@gmail.com>
2023-11-07 11:59:41 -0500

    TODO: wip exclude splash

--- bioc_webstats/splash.py
+++ bioc_webstats/splash.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """User views."""
 from flask import Blueprint, render_template
-#from bioc_webstats.stats import URI_PATH_PREFIX
 import os
+from bioc_webstats.app import register_blueprints
 
 # Check if the application is in production mode
 is_production = os.environ.get('FLASK_ENV') == 'production'
@@ -10,6 +10,9 @@ is_production = os.environ.get('FLASK_ENV') == 'production'
 # TODO @n1khilmane here is were the prefix goes argument url_prefix 
 # do we need this url_prefix ? since its a home page
 blueprint = Blueprint("spash", __name__)
+
+if not is_production:
+    register_blueprints(blueprint)
 
 # TODO @n1khilmane Exclude this from prod
 @blueprint.route("/")
@@ -27,8 +30,5 @@ def home():
         "/bioc/affy/affy_stats.tab",
         "/bioc/affy/affy_2023_stats.tab",
     ]
-
-    if is_production:
-        return "This route is not available in production."
 
     return render_template("home.html", targets=targets)
