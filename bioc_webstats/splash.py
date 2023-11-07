f29685fefce1421002edfa7961082cff71adb4d2
n1khilmane <nikhilmane1919@gmail.com>
2023-11-07 15:45:31 -0500

    test splash.py

--- bioc_webstats/splash.py
+++ bioc_webstats/splash.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 """User views."""
 from flask import Blueprint, render_template
+import os
 
-# TODO @n1khilmane here is were the prefix goes argument url_prefix 
-# do we need this url_prefix ? since its a home page
-blueprint = Blueprint("spash", __name__)
+is_production = os.environ.get('FLASK_ENV') == 'production'
+
+if not is_production:
+    blueprint = Blueprint("spash", __name__)
 
 
-# TODO @n1khilmane Exclude this from prod
-@blueprint.route("/")
-def home():
-    """Home page for testing only."""
-    targets = [
-        "/",
-        "/data-experiment.html",
-        "/bioc/",
-        "/bioc/bioc_packages.txt",
-        "/bioc/bioc_2023_stats.tab",
-        "/bioc/bioc_pkg_scores.tab",
-        "/bioc/bioc_pkg_stats.tab",
-        "/bioc/affy/",
-        "/bioc/affy/affy_stats.tab",
-        "/bioc/affy/affy_2023_stats.tab",
-    ]
+    @blueprint.route("/")
+    def home():
+        """Home page for testing only."""
+        targets = [
+            "/",
+            "/data-experiment.html",
+            "/bioc/",
+            "/bioc/bioc_packages.txt",
+            "/bioc/bioc_2023_stats.tab",
+            "/bioc/bioc_pkg_scores.tab",
+            "/bioc/bioc_pkg_stats.tab",
+            "/bioc/affy/",
+            "/bioc/affy/affy_stats.tab",
+            "/bioc/affy/affy_2023_stats.tab",
+        ]
 
-    return render_template("home.html", targets=targets)
+        return render_template("home.html", targets=targets)
