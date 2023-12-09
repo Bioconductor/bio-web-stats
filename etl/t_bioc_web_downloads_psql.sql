5e2bd7445bb148bc4ea3b35b71a1c0a0353dc74e
Robert Shear <rshear@gmail.com>
2023-12-09 20:29:46 +0000

    bump c-ip to varchar(40) for v6

--- etl/t_bioc_web_downloads_psql.sql
+++ etl/t_bioc_web_downloads_psql.sql
@@ -2,7 +2,7 @@
 -- For postgresql. databasse webstats
 CREATE TABLE "bioc_web_downloads"(
   "date" date, 
-  "c-ip" varchar(16), 
+  "c-ip" varchar(40), 
   "sc-status" INTEGER, 
   "category" varchar(16), 
   "package" varchar(64))
