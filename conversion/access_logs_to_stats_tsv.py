43cb5363fe70375958369c352d8b888ce83d3a01
Robert Shear <rshear@gmail.com>
2023-12-09 22:16:26 +0000

    db conversion accomodate column name change in 2019

--- conversion/access_logs_to_stats_tsv.py
+++ conversion/access_logs_to_stats_tsv.py
@@ -8,32 +8,6 @@ from datetime import datetime as dt
 
 from psql_connection import psql_get_connection
 
-sql_select_command = """
-select strftime(
-        '%Y-%m-%d',
-        substr(day_month_year, 8, 4) || '-' || case
-            substr(day_month_year, 4, 3)
-            when 'Jan' then '01'
-            when 'Feb' then '02'
-            when 'Mar' then '03'
-            when 'Apr' then '04'
-            when 'May' then '05'
-            when 'Jun' then '06'
-            when 'Jul' then '07'
-            when 'Aug' then '08'
-            when 'Sep' then '09'
-            when 'Oct' then '10'
-            when 'Nov' then '11'
-            when 'Dec' then '12'
-        end || '-' || substr(day_month_year, 1, 2)
-    ) AS "date",
-    ips as "c-ip",
-    statuscode as "sc-status",
-    biocrepo as "category",
-    "package"
-from access_log
-"""
-
 
 def export_chunked_tsv(db_path, query, chunk_size, start_at=0):
     """Export the table in chunks."""
@@ -75,6 +49,32 @@ chunk_size = 1000000
 
 for year in range(2009, 2021):
     source_db = f'/mnt/data/home/biocadmin/download_dbs/download_db_{year}.sqlite'
+    status_colname = 'errorcode' if year < 2019 else 'statuscude'
+    sql_select_command = f"""
+        select strftime(
+                '%Y-%m-%d',
+                substr(day_month_year, 8, 4) || '-' || case
+                    substr(day_month_year, 4, 3)
+                    when 'Jan' then '01'
+                    when 'Feb' then '02'
+                    when 'Mar' then '03'
+                    when 'Apr' then '04'
+                    when 'May' then '05'
+                    when 'Jun' then '06'
+                    when 'Jul' then '07'
+                    when 'Aug' then '08'
+                    when 'Sep' then '09'
+                    when 'Oct' then '10'
+                    when 'Nov' then '11'
+                    when 'Dec' then '12'
+                end || '-' || substr(day_month_year, 1, 2)
+            ) AS "date",
+            ips as "c-ip",
+            {status_colname} as "sc-status",
+            biocrepo as "category",
+            "package"
+        from access_log
+        """
     print('start at ' +dt.now().strftime("%Y-%m-%d %H:%M:%S"))
     record_count = export_chunked_tsv(source_db, sql_select_command, chunk_size)
     print('end at ' +dt.now().strftime("%Y-%m-%d %H:%M:%S") + " Total records=" + str(record_count))
