810985615da687e8f492de38aca05e66b8a3a232
Robert D Shear <rshear@gmail.com>
2023-12-11 07:30:29 -0500

    debug v_stats vrew for postgres

--- etl/postgresql/v_stats_psqlsql.sql
+++ etl/postgresql/v_stats_psqlsql.sql
@@ -1,26 +1,25 @@
--- create view v_stats as
+create view v_stats as
 with T as (select category,
     package,
-    year("date") "yr",
+    DATE_TRUNC('year', "date") "yr",
     count(distinct "c-ip") ip_count,
     count(*) download_count
 from bioc_web_downloads
 group by category,
     package,
-    year("date")
+    DATE_TRUNC('year', "date")
 )
 select "category", "package", 
-    CAST(CAST(yr AS VARCHAR) || '-12-31' AS DATE) "date", 
-    false as is_monthly,
+    yr + INTERVAL '1 year' - INTERVAL '1 day' AS "date",
+    cast('false' as BOOLEAN) as is_monthly,
     "ip_count", 
     "download_count" 
 from T
 UNION ALL
 select category,
     package,
-    DATE_TRUNC('year', "date") + INTERVAL '1 year' - INTERVAL '1 day' AS date
     date_trunc('MONTH', "date") "date", 
-    true as is_monthly,
+    cast('true' as BOOLEAN) as is_monthly,
     count(distinct "c-ip") ip_count,
     count(*) download_count
 from bioc_web_downloads
