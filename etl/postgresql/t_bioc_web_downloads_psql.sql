c3d9dee617f55bdd32885e2a1d1f8d9fa7eb8226
Robert D Shear <rshear@gmail.com>
2023-12-11 14:22:40 -0500

    postgres DDL mods

--- etl/postgresql/t_bioc_web_downloads_psql.sql
+++ etl/postgresql/t_bioc_web_downloads_psql.sql
@@ -1,10 +1,28 @@
+-- Table: public.bioc_web_downloads
 
--- For postgresql. databasse webstats
-CREATE TABLE "bioc_web_downloads"(
-  "date" date, 
-  "c-ip" varchar(40), 
-  "sc-status" INTEGER, 
-  "category" varchar(16), 
-  "package" varchar(64))
+-- DROP TABLE IF EXISTS public.bioc_web_downloads;
 
-CREATE INDEX idx_date ON bioc_web_downloads ("date", "c-ip", "category", "package")
+CREATE TABLE IF NOT EXISTS public.bioc_web_downloads
+(
+    date date,
+    "c-ip" character varying(40) COLLATE pg_catalog."default",
+    "sc-status" integer,
+    category character varying(16) COLLATE pg_catalog."default",
+    "package" character varying(64) COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.bioc_web_downloads
+    OWNER to postgres;
+-- Index: idx_date
+
+-- DROP INDEX IF EXISTS public.idx_date;
+
+CREATE INDEX IF NOT EXISTS idx_date
+    ON public.bioc_web_downloads USING btree
+    (date ASC NULLS LAST, "c-ip" COLLATE pg_catalog."default" ASC NULLS LAST, category COLLATE pg_catalog."default" ASC NULLS LAST, package COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.bioc_web_downloads
+    CLUSTER ON idx_date;
