ef08b18cdcb1b4c91e95b7b98380aafa18362ed2
Robert Shear <rshear@gmail.com>
2023-12-19 15:10:15 -0500

    packages table with 4 columns

--- etl/postgresql/t_packages_postgres.sql
+++ etl/postgresql/t_packages_postgres.sql
@@ -4,8 +4,11 @@
 
 CREATE TABLE IF NOT EXISTS public.packages
 (
+    category character varying(16) COLLATE pg_catalog."default" NOT NULL,
     "package" character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT packages_pkey PRIMARY KEY ("package")
+    first_version character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    last_version character varying(8) COLLATE pg_catalog."default",
+    CONSTRAINT packages_pkey PRIMARY KEY (category, "package")
 )
 
 TABLESPACE pg_default;
