f81c7bb666173b4aee5494806622adf0cb5a6bce
Robert D Shear <rshear@gmail.com>
2023-12-22 06:37:33 -0500

    prototype server control files

---
+++ supervisord_programs/start_gunicorn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/ubuntu/bio-web-stats
+. .venv/bin/activate
+gunicorn --bind 0.0.0.0:8000 --log-level debug -w 4 \
+    --access-logfile /var/log/bioc-webstats/access.log \
+    --error-logfile /var/log/bioc-webstats/error.log \
+    'bioc_webstats.app:create_app()'
