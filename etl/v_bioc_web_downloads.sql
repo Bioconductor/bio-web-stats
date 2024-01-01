9944ebbbc98495c5279369c5abb0a30663e87af6
Robert D Shear <rshear@gmail.com>
2024-01-01 16:42:02 -0500

    modify the v_bioc_web_downloads view for production (names from cloudfront_logs)

--- etl/v_bioc_web_downloads.sql
+++ etl/v_bioc_web_downloads.sql
@@ -1,41 +1,42 @@
 /*
 view bioc_web_downloads
-package and categories are inferred from the cs-uri-stem
+package and categories are inferred from the uri
 Select only package downloads, which must have a valid category and end with a package tarball
 The package tarball must be in the form 
     :<package_name>_<version>.tar.<compression>,
     where compression is gz, zip, or tgz
-The category is extracted from the start of the cs-uri-stem, and must be in the form:
+The category is extracted from the start of the uri, and must be in the form:
     /packages/<version>/<category>/...
 Valid categories are: bioc, workflows, data/experiment, data/annotation
 
 Note: Wherever a '//' appears in the uri-stem, it is treated as a single '/'
+Note: standard name for uri is "cs-uri-stem"
 Note: The RE string is repeated multiple times becasue Athena does not have reasonable way
 to handle manifest constants (as of Oct-2023) 
 */
 
-CREATE OR REPLACE VIEW "bioc_web_downloads" AS
+CREATE OR REPLACE VIEW "v_bioc_web_downloads" AS
 with T as (
 SELECT "date",
-    "c-ip",
-    "sc-status",
+    request_ip as "c-ip",
+    "status" as "sc-status",
     replace(regexp_extract(
-        "cs-uri-stem",
+        "uri",
         '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\.(?:tar|gz|zip|tgz)$',
         1
     ), 'data/', '') category,
     regexp_extract(
-        "cs-uri-stem",
+        "uri",
         '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\.(?:tar|gz|zip|tgz)$',
         2
     ) package
-FROM "bioc_web_logs"
+FROM "cloudfront_logs"
 WHERE (
         (
-            "sc-status" BETWEEN 200 AND 399
+            "status" BETWEEN 200 AND 399
         )
         AND regexp_like(
-            "cs-uri-stem",
+            "uri",
         '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\.(?:tar|gz|zip|tgz)$'
         )
     )
