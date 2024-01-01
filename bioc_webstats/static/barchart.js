d0adb774e29da8b2961864e8d5eb77de5827647a
Robert D Shear <rshear@gmail.com>
2024-01-01 11:59:23 -0500

    chart styling and y-scale log10

--- bioc_webstats/static/barchart.js
+++ bioc_webstats/static/barchart.js
@@ -1,54 +1,64 @@
 function plot_bars(canvas_id, year, data) {
-    var ctx = document
-        .getElementById(canvas_id)
-        .getContext("2d");
-    var gradient = ctx.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, 'rgba(255, 0, 0, 0.5)');
-    gradient.addColorStop(1, 'rgba(0, 0, 255, 0.5)');
+  var ctx = document.getElementById(canvas_id).getContext("2d");
+  var gradient = ctx.createLinearGradient(0, 0, 0, 400);
+  gradient.addColorStop(0, "rgba(255, 0, 0, 0.5)");
+  gradient.addColorStop(1, "rgba(0, 0, 255, 0.5)");
 
-    // Initialize empty arrays for each element
-    const downloadsList = [];
-    const monthList = [];
-    const uniqueIpsList = [];
+  const downloadsList = [];
+  const monthList = [];
+  const uniqueIpsList = [];
 
-    // Define the maximum number of rows to iterate
-    const maxRows = Math.min(data.length, 12);
+  const maxRows = Math.min(data.length, 12);
 
-    // Iterate through the data array and populate the lists
-    for (let i = 0; i < maxRows; i++) {
-        downloadsList.push(data[i].downloads);
-        monthList.push(data[i].month);
-        uniqueIpsList.push(data[i].unique_ips);
-    }
-
-    var unique_ip = [1,2,3,4,5,6,7,8,9,10,11,12];
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: monthList,
-            datasets: [{
-                label: 'Unique IPs',
-                data: uniqueIpsList,
-                backgroundColor: gradient,
-                borderWidth: 1
-            },
-            {
-                label: 'Downloads',
-                data: downloadsList,
-                backgroundColor: gradient,
-                borderWidth: 1
-            }]
+  for (let i = 0; i < maxRows; i++) {
+    downloadsList.push(data[i].downloads);
+    monthList.push(data[i].month);
+    uniqueIpsList.push(data[i].unique_ips);
+  }
+  var theChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: monthList,
+      datasets: [
+        {
+          label: "Unique IPs",
+          data: uniqueIpsList,
+          backgroundColor: '#aaaaff',
+          borderWidth: 1,
         },
-        options: {
-        }
-    });
-};
+        {
+          label: "Downloads",
+          data: downloadsList,
+          backgroundColor: '#ddddff',
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          type: "logarithmic",
+          min: 10,
+          ticks: {
+            // Generate the labels for each power of ten
+            callback: function (value, index, values) {
+              // Return the value if it's an exact power of ten
+              if (Math.log10(value) % 1 === 0) {
+                return value.toString();
+              }
+            },
+          },
+        },
+      },
+    },
+  });
+}
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const elements = document.getElementsByClassName('barchart');
-    for (let i = 0; i < elements.length; i++) {
-        const canvas_id = elements[i].id;
-        const year = canvas_id.replace('barchart_', '');
-        plot_bars(canvas_id, year, data_table[year]);
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+  const elements = document.getElementsByClassName("barchart");
+  for (let i = 0; i < elements.length; i++) {
+    const canvas_id = elements[i].id;
+    const year = canvas_id.replace("barchart_", "");
+    plot_bars(canvas_id, year, data_table[year]);
+  }
 });
