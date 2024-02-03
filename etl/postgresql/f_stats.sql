48578c5f5e275cca902abc307ec394b8dc1fb205
Robert D Shear <rshear@gmail.com>
2024-02-03 17:59:13 -0500

    ETL sql refactoring continued WIP

--- etl/postgresql/f_stats.sql
+++ etl/postgresql/f_stats.sql
@@ -1,52 +1,68 @@
--- FUNCTION: public.f_stats(date, boolean)
- -- DROP FUNCTION IF EXISTS public.f_stats(date, boolean);
+-- FUNCTION: public.f_stats(date, boolean, boolean)
 
-CREATE OR REPLACE FUNCTION PUBLIC.F_STATS(START_DATE date, MONTHLY boolean) 
-RETURNS TABLE(CATEGORY CHARACTER varying, 
-			  PACKAGE CHARACTER varying, 
-			  "date" date, 
-			  IS_MONTHLY boolean,
-			  IP_COUNT bigint, 
-			  DOWNLOAD_COUNT bigint
-			 ) 
-	 LANGUAGE 'plpgsql' 
-	 COST 100 
-	 VOLATILE PARALLEL UNSAFE 
+-- DROP FUNCTION IF EXISTS public.f_stats(date, boolean, boolean);
+
+CREATE OR REPLACE FUNCTION public.f_stats(
+	start_date date,
+	monthly boolean,
+	by_package boolean)
+    RETURNS TABLE(category character varying, package character varying, date timestamp without time zone, is_monthly boolean, ip_count bigint, download_count bigint) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
 AS $BODY$
+DECLARE
+	adjusted_start_date DATE;
 BEGIN
+	adjusted_start_date := 
+		CASE
+			WHEN monthly
+			THEN
+				date_trunc('MONTH'::text, start_date::timestamp without time zone)
+			ELSE
+				date_trunc('year', start_date::timestamp without time zone)
+		END;
+
     RETURN QUERY -- Use RETURN QUERY to return a set of rows
-    WITH t AS (
         SELECT d.category,
-            d.package,
+			CASE
+				WHEN by_package
+				THEN d.package
+				ELSE '*'
+			END as package,
 			CASE
 				WHEN monthly
 				THEN
+					date_trunc('MONTH'::text, D.date::timestamp without time zone)
+				ELSE
 					date_trunc('year', d.date::timestamp without time zone) + 
 						'1 year'::interval - '1 day'::interval
-				ELSE
-					date_trunc('MONTH'::text, D.date::timestamp)
-			END AS dt,
-            "c-ip",
-            download_count
+			END AS "date",
+			MONTHLY as is_monthly,
+			COUNT(DISTINCT "c-ip") ip_count,
+			count(*) as download_count
         FROM bioc_web_downloads d
         JOIN packages p ON d.package::text = p.package::text AND d.category::text = p.category::text
-        WHERE d.date > start_date
-    )
-    SELECT t.category,
-        t.package,
-		t.dt::date as date,
-        monthly as is_monthly,
-        COUNT(DISTINCT t."c-ip") ip_count,
-        count(*) as download_count
-    FROM t
-	GROUP BY t.category, t.package, t.dt
+        WHERE d.date >= adjusted_start_date
+		GROUP BY d.category, 
+			CASE
+				WHEN by_package
+				THEN d.package
+				ELSE '*'
+			END,
+			CASE
+				WHEN monthly
+				THEN
+					date_trunc('MONTH'::text, D.date::timestamp without time zone)
+				ELSE
+					date_trunc('year', d.date::timestamp without time zone) + '1 year'::interval - '1 day'::interval
+			END
+
 ;
 END;
 $BODY$;
 
-
-ALTER FUNCTION PUBLIC.F_STATS(date, boolean) OWNER TO POSTGRES;
-
-
-SELECT *
-FROM F_STATS('2023-01-01', TRUE) where package = 'affy';
+ALTER FUNCTION public.f_stats(date, boolean, boolean)
+    OWNER TO postgres;
--- etl/postgresql/t_stats_postgres.sql
+++ etl/postgresql/t_stats_postgres.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS public.stats
 (
     category character varying(16) COLLATE pg_catalog."default",
     "package" character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    date date NOT NULL,
+    "date" date NOT NULL,
     is_monthly boolean NOT NULL,
     ip_count integer,
     download_count integer,
-    CONSTRAINT stats_pkey PRIMARY KEY ("package", date, is_monthly)
+    CONSTRAINT stats_pkey PRIMARY KEY ("package", "date")
 )
 
 TABLESPACE pg_default;
