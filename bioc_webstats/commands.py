1e48c5a31e87c4270691de51595d50613bbbc90f
Robert D Shear <rshear@gmail.com>
2024-03-14 16:04:20 -0400

    ingest and parameter store mods WIP

--- bioc_webstats/commands.py
+++ bioc_webstats/commands.py
@@ -5,6 +5,7 @@ from glob import glob
 from subprocess import call
 from flask import current_app
 import logging
+from bioc_webstats.ingest_logs import ingest_logs
 
 import click
 
@@ -90,49 +91,4 @@ def gendb():
 
 @click.command()
 def ingest():
-    """Ingest download logs from Athena.
-    See https://aws-sdk-pandas.readthedocs.io/en/latest/index.html
-    """
-    from datetime import date, timedelta
-    from sqlalchemy import create_engine, text
-    from bioc_webstats.models import WebstatsInfo
-    import pandas as pd
-    import awswrangler as wr
-    
-    db_connection_string = current_app.config["SQLALCHEMY_DATABASE_URI"]
-    current_app.logger.log(logging.INFO, 'Preparing to open <%s> database', db_connection_string)
-#    source_connection_string = "s3://bioc-webstats-download-logs/data/year=2024/month=01/day=10/"  # TODO current_app.config["SOURCE LOCATION"]
-#    df = wr.s3.read_parquet(source_connection_string, dataset=True)
-    engine = create_engine(db_connection_string)
-    with engine.connect() as conn:
-        # TODO Detect and report error here --- DB not available
-        start_date = WebstatsInfo.get_valid_thru_date()
-    query_str = f"""
-    WITH
-  T AS (
-   SELECT
-     "date"
-   , request_ip "c-ip"
-   , "status" "sc-status"
-   , replace(regexp_extract("uri", '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\\.(?:tar|gz|zip|tgz)$', 1), 'data/', '') category
-   , regexp_extract("uri", '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\\.(?:tar|gz|zip|tgz)$', 2) package
-   , LPAD(CAST(year("date") AS VARCHAR), 4, '0') "year"
-   , LPAD(CAST(month("date") AS VARCHAR), 2, '0') "month"
-   , LPAD(CAST(day("date") AS VARCHAR), 2, '0') "day"
-   FROM
-     "cloudfront_logs"
-   WHERE
-    "date" > DATE '{start_date}' AND
-    "status" in (200, 301, 302, 307, 308) AND 
-    regexp_like("uri", '^/+packages/+[^/]*/+(bioc|workflows|data/+experiment|data/+annotation)/+(?:bin|src)/+(?:[^/]*/+)*([^_]*)_.*\\.(?:tar|gz|zip|tgz)$'))
-    SELECT * FROM T WHERE (package <> '')
-    LIMIT 10;
-"""
-    database = "glue-sup-db"
-    output = "s3://perf-anal-2022-12-06-rds/"
-    result = wr.athena.read_sql_query(sql=query_str, database=database, ctas_approach=True)
-    # TODO do I need to move it to parquet first?
-    pass
-    # foo = df.to_sql('bioc_web_downloads', engine)
-    pass
-
+    ingest_logs()
--- bioc_webstats/settings.py
+++ bioc_webstats/settings.py
@@ -12,12 +12,27 @@ import boto3
 env = Env()
 env.read_env()
 # TODO Raise a FATAL error if no .env file is found.
-AWS_PARAMETER_PATH = env.str('AWS_PARAMETER_PATH', None)
-if env.str('AWS_PARAMETER_PATH', None) is not None:
-    ssm_client = boto3.client('ssm')
-    plist = ssm_client.get_parameters_by_path(Path = AWS_PARAMETER_PATH, Recursive=True)
-    param_dict = {item["Name"][len(AWS_PARAMETER_PATH)+1:] : item["Value"] for item in plist["Parameters"]}
-    pass
+
+# If AWS_ROLE is defined, then set the role and get the AWS parameters
+AWS_ROLE = env.str('AWS_ROLE', None)
+
+# If the environment variable AWS_ROLE exists, then get the parameters from SSM parameter Store
+
+if AWS_ROLE is not None:
+    
+    # If AWS_ROLE is not the null string, then use the value to set the role name with STS
+    if AWS_ROLE != '':
+            
+
+    AWS_PARAMETER_PATH = env.str('AWS_PARAMETER_PATH', None)
+
+    if env.str('AWS_PARAMETER_PATH', None) is not None:
+        ssm_client = boto3.client('ssm')
+        plist = ssm_client.get_parameters_by_path(Path = AWS_PARAMETER_PATH, Recursive=True)
+        param_dict = {item["Name"][len(AWS_PARAMETER_PATH)+1:] : item["Value"] for item in plist["Parameters"]}
+        pass
+
+    # TODO PROCESS param_dict and store in variables as appropriate
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
