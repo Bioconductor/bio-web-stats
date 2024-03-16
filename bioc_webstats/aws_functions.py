f308ef1b364a77f191fc04b9cc284728dc177b01
Robert D Shear <rshear@gmail.com>
2024-03-16 10:11:46 -0400

    ingest and parameter store mods

--- bioc_webstats/aws_functions.py
+++ bioc_webstats/aws_functions.py
@@ -1,2 +1,35 @@
+''' aws_functions TODO rename this.'''
 import boto3
 from botocore.exceptions import ClientError
+
+def aws_assume_sts_role(role_arn, role_session_name):
+
+    # Create an STS client
+    sts_client = boto3.client('sts')
+
+    # Assume the specified role
+    assumed_role_object = sts_client.assume_role(
+        RoleArn=role_arn,
+        RoleSessionName=role_session_name
+    )
+
+    # Credentials to be used for the session with the assumed role
+    credentials = assumed_role_object['Credentials']
+    return credentials
+
+    # # Use the temporary credentials that AssumeRole returns to make a connection to Amazon S3
+    # s3_resource = boto3.resource(
+    #     's3',
+    #     aws_access_key_id=credentials['AccessKeyId'],
+    #     aws_secret_access_key=credentials['SecretAccessKey'],
+    #     aws_session_token=credentials['SessionToken'],
+    # )
+
+    # Now you can use the s3_resource object to interact with S3
+    
+if __name__ == "__main__":
+    result = aws_assume_sts_role(
+        'arn:aws:iam::931729544676:role/bioc-webstats-webrunner',
+        'webstats.tester')
+    pass
+    
--- bioc_webstats/settings.py
+++ bioc_webstats/settings.py
@@ -5,39 +5,43 @@ Most configuration is set via environment variables.
 
 For local development, use a .env file to set
 environment variables.
+
+See https://pypi.org/project/environs/ for more information.
 """
+import os
 from environs import Env
 import boto3
 
+def get_str(target, default):
+    """Get parameter from SSM parameter store if present, otherwise from Env()"""
+    if target in param_dict:
+        return param_dict[target]
+    return env.str(target, default)
+
 env = Env()
 env.read_env()
-# TODO Raise a FATAL error if no .env file is found.
 
-# If AWS_ROLE is defined, then set the role and get the AWS parameters
-AWS_ROLE = env.str('AWS_ROLE', None)
-
-# If the environment variable AWS_ROLE exists, then get the parameters from SSM parameter Store
-
-if AWS_ROLE is not None:
-    
-    # If AWS_ROLE is not the null string, then use the value to set the role name with STS
-    if AWS_ROLE != '':
-            
-
-    AWS_PARAMETER_PATH = env.str('AWS_PARAMETER_PATH', None)
-
-    if env.str('AWS_PARAMETER_PATH', None) is not None:
-        ssm_client = boto3.client('ssm')
+# Do we have a parameter set in AWS SSM Parameter Store?
+AWS_PARAMETER_PATH = env.str('AWS_PARAMETER_PATH', None)
+if env.str('AWS_PARAMETER_PATH', None) is  None:
+    param_dict = {}
+else:
+    # Yes Add the values to the parameter set
+    ssm_client = boto3.client('ssm')
+    plist = ssm_client.get_parameters_by_path(Path = AWS_PARAMETER_PATH, Recursive=True)
+    # HACK Start from the top with SSM paramater store included
+    for item in plist["Parameters"]:
         plist = ssm_client.get_parameters_by_path(Path = AWS_PARAMETER_PATH, Recursive=True)
         param_dict = {item["Name"][len(AWS_PARAMETER_PATH)+1:] : item["Value"] for item in plist["Parameters"]}
-        pass
 
-    # TODO PROCESS param_dict and store in variables as appropriate
+# TODO Refactor Parameter store config variables to match local
+# TODO Complete parameters
+# TODO Secret manager for database values
 
-ENV = env.str("FLASK_ENV", default="production")
-DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
-SECRET_KEY = env.str("SECRET_KEY")
+ENV = get_str("FLASK_ENV", default="production")
+DEBUG = get_str('DEBUG', ENV == "development")
+SQLALCHEMY_DATABASE_URI = get_str("DATABASE_URL")
+SECRET_KEY = get_str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
