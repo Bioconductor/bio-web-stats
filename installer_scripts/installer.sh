46c8efb428f1d6a40e87d900cae6100e67040d57
Robert Shear <rshear@gmail.com>
2024-07-01 14:41:55 -0400

    webstats installer generaization WIP

--- installer_scripts/installer.sh
+++ installer_scripts/installer.sh
@@ -45,17 +45,28 @@ sudo chown -R ubuntu /var/log/bioc-webstats
 sudo mkdir -p /var/www/bioc-webstats
 sudo chown -R ubuntu /var/www/bioc-webstats
 
-# TODO parameterize. Note now file flask_environment
 export FLASK_APP="bioc_webstats.app:create_app('production', '/bioc/webstats/prod')"
 export FLASK_AWS_PATH_PARAMETER=/bioc/webstats/prod
-export FLASK_APPROOT=/home/ubuntu/
+export FLASK_OSUSER="webstats"
+export FLASK_OSGROUP="webstats"
+export FLASK_APPROOT="/var/www/bioc-webstats"
+# TODO need configmodule entry for FLASK_LOGROOT
+export FLASK_LOGROOT="/var/log/bioc-webstats"
+export AWS_PARAMETER_PATH='/bioc/webstats/prod'
+
+sudo mkdir -p $FLASK_APPROOT
+sudo chown -R $FLASK_OSUSER $FLASK_APPROOT
+
+sudo mkdir -p $FLASK_LOGROOT
+sudo chown -R $FLASK_OSUSER $FLASK_LOGROOT
 
 
-# TODO put this in python -m bioc_webstats.post_install
+# TODO put this in python -m bioc_webstats.post_install with generation of flask_environment and bioc-webstats.service
 sudo cp ~/.venv/lib/python3.12/site-packages/supervisord_programs/bioc-webstats.service /etc/systemd/system/
-sudo cp ~/.venv/lib/python3.12/site-packages/supervisord_programs/flask_environment /home/ubuntu
-sudo chmod +x /home/ubuntu/flask_environment
+sudo cp ~/.venv/lib/python3.12/site-packages/supervisord_programs/flask_environment $FLASK_APPROOT
+sudo chown $FLASK_OSUSER:$FLASK_OSGROUP $FLASK_APPROOT
+sudo chmod +x $FLASK_APPROOT
 sudo systemctl enable bioc-webstats.service
 sudo systemctl start bioc-webstats.service
 sudo systemctl status bioc-webstats.service
-# TODO move flask_ingest.sh and flask_ingest_rontabs_setup.sh to target machiine
+# TODO move flask_ingest.sh and flask_ingest_crontabs_setup.sh to target machiine
